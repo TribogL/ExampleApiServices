9e4a569c822ea862e53c326ffa7d96badeb758d8
Tribog <julix4212@gmail.com>
2024-10-03 08:08:37 -0500

    feat: teh controllers Delete and put were created.

--- Controllers/V1/Vehicles/VehiclesController.cs
+++ Controllers/V1/Vehicles/VehiclesController.cs
@@ -32,7 +32,7 @@ public class VehiclesController : ControllerBase
     public async Task<ActionResult<Vehicle>> GetById(int id)
     {
         var Vehicle = await _context.Vehicles.FindAsync(id);
-        if(Vehicle == null)
+        if (Vehicle == null)
         {
             return NotFound();
         }
@@ -42,16 +42,65 @@ public class VehiclesController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<Vehicle>> Create(VehicleDTO inputvehicle)
     {
-        if(!ModelState.IsValid)
+        if (!ModelState.IsValid)
         {
             return BadRequest(ModelState);
         }
 
-        var  newVehicle = new Vehicle(inputvehicle.Make, inputvehicle.Model, inputvehicle.Year, inputvehicle.Price, inputvehicle.Color);
+        var newVehicle = new Vehicle(inputvehicle.Make, inputvehicle.Model, inputvehicle.Year, inputvehicle.Price, inputvehicle.Color);
 
         _context.Vehicles.Add(newVehicle);
         await _context.SaveChangesAsync();
-        return CreatedAtAction(nameof(GetById), new {id = newVehicle.Id}, inputvehicle);
+        return CreatedAtAction(nameof(GetById), new { id = newVehicle.Id }, inputvehicle);
+    }
+
+    [HttpPut("{id}")]
+
+    public async Task<IActionResult> Update(int id, VehicleDTO updateVehicle)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        var Vehicle = await _context.Vehicles.FindAsync(id);
+        if (Vehicle == null)
+        {
+            return NotFound();
+        }
+        Vehicle.Make = updateVehicle.Make;
+        Vehicle.Model = updateVehicle.Model;
+        Vehicle.Year = updateVehicle.Year;
+        Vehicle.Price = updateVehicle.Price;
+        Vehicle.Color = updateVehicle.Color;
+
+        _context.Vehicles.Update(Vehicle);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+
+    }
+
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Vehicle>> Delete(int id)
+    {
+        var Vehicle = await _context.Vehicles.FindAsync(id);
+        if (Vehicle == null)
+        {
+            return NotFound();
+        }
+        _context.Vehicles.Remove(Vehicle);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpGet("{id}/exists")]
+
+    public async Task<ActionResult<bool>> Exists(int id)
+    {
+        var exists = await _context.Vehicles.AnyAsync(v => v.Id == id);
+        return Ok(exists);
     }
 
 }
